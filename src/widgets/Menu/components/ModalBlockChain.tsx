fccb8e00aded847b03c4155bbd467ff436085f73
yose216 <youness.chetoui@gmail.com>
2021-09-30 17:23:41 +0200

    add rsk logo

--- src/widgets/Menu/components/ModalBlockChain.tsx
+++ src/widgets/Menu/components/ModalBlockChain.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react'
 import Button from "../../../components/Button/Button";
 import Text from "../../../components/Text/Text";
-import { BSCIcon } from "../icons";
+import { BSCIcon, RSKIcon } from "../icons";
 import { Modal } from "../../Modal";
 
 
@@ -32,11 +32,12 @@ const ModalBlockChain: React.FC<ModalBlockChainProps> = ({ onDismiss }) => {
         onClick={() => {console.log('1')}}
         style={{ justifyContent: "space-between" }}
         mb={10}
-        disabled={true}
       >
         <Text bold color="primary" mr="16px">
-          RSK (soon)
+          RSK
         </Text>
+        <RSKIcon width="32px" height="32px" />
+
       </Button>
       <Button
         width="100%"
